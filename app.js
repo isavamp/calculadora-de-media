cbd790271f799c319dc432801252503df49105f8
isavamp <isadora.7791@colegioprogresso.g12.br>
2026-03-04 11:22:35 -0300

    pegamos os dados

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-("sua linda")
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+
+const button = document.getElementById("button")
+
+function calcularMedia(){
+    const valorNota1 = Number (nota1.value)
+    const valorNota2 = Number (nota2.value)
+    const media = (valorNota1 + valorNota2)/2
+    console.log(media)
+}
+
+button.addEventListener("click", calcularMedia)
