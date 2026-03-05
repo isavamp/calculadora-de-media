5e894834e87441ca8189168a7205b227cc7fa722
isavamp <isadora.7791@colegioprogresso.g12.br>
2026-03-05 07:49:46 -0300

    finalizamos a media

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const nota1 = document.getElementById("nota1")
 const nota2 = document.getElementById("nota2")
+const resultado = document.getElementById("resultado")
 
 const button = document.getElementById("button")
 
@@ -7,7 +8,7 @@ function calcularMedia(){
     const valorNota1 = Number (nota1.value)
     const valorNota2 = Number (nota2.value)
     const media = (valorNota1 + valorNota2)/2
-    console.log(media)
+    resultado.innerText = `A Média é ${media.toFixed(2)}`
 }
 
 button.addEventListener("click", calcularMedia)
